cac405eb6ad332a17fd015cd0ef39c0947d9b5af
Dmitry Chestnykh <dmitry@codingrobots.com>
2015-04-24 14:51:40 +0200

    VaryBy* funcs should return a func.

--- varyby.go
+++ varyby.go
@@ -5,19 +5,30 @@ import (
 	"net/http"
 )
 
-// VaryByIP is a custom "vary by" function for throttled, which varies request
-// based on client's IP address from the given header.  If header is empty,
-// extracts IP address from r.RemoteAddr.
-func VaryByIP(r *http.Request, headerName string) string {
+// VaryByIP returns a custom "vary by" function for throttled, which varies
+// request based on client's IP address from the given header.  If header is
+// empty, extracts IP address from r.RemoteAddr.
+func VaryByIP(headerName string) func(r *http.Request) string {
+	return func(r *http.Request) string {
+		return getRequestIP(r, headerName)
+	}
+}
+
+// VaryByPathAndIP is like VaryByIP but also adds request path.
+func VaryByPathAndIP(headerName string) func(r *http.Request) string {
+	return func(r *http.Request) string {
+		return r.URL.Path + "\n" + getRequestIP(r, headerName)
+	}
+}
+
+// getRequestIP returns a remote IP address of the client that made the
+// request. The address is take from the given header name, or from RemoteAddr
+// if the header name is an empty string.
+func getRequestIP(r *http.Request, headerName string) string {
 	if headerName == "" {
 		return extractIP(r.RemoteAddr)
 	}
 	return extractIP(r.Header.Get(headerName))
-}
-
-// VaryByPathAndIP is like VaryByIP but also adds request path.
-func VaryByPathAndIP(r *http.Request, headerName string) string {
-	return r.URL.Path + "\n" + VaryByIP(r, headerName)
 }
 
 // extractIP extracts IP address (or host) from the given string,
